2ba7a09e55fadacdd7db744b1bb081b8ec08a3b8
Dimitris <dimitris13k@gmail.com>
2018-11-10 20:02:01 +0000

    Refactor newGame function

--- new.go
+++ new.go
@@ -5,10 +5,16 @@ import "github.com/gorilla/websocket"
 func newGame(gameCounter *int, games map[int]*Game, ws *websocket.Conn) {
 	*gameCounter++
 
-	game := new(Game)
-	game.GameId = *gameCounter
-	game.Players[0] = ws
+	game := createNewGame(gameCounter, ws)
 	games[*gameCounter] = game
 
 	sendWsMessage(ws, game)
+}
+
+func createNewGame(gameCounter *int, ws *websocket.Conn) *Game {
+	game := new(Game)
+	game.GameId = *gameCounter
+	game.Players[0] = ws
+
+	return game
 }
