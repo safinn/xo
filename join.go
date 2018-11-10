1a61adffe414d2ae5c4f55496a6c8ec248b764d7
Dimitris <dimitris13k@gmail.com>
2018-11-10 20:43:44 +0000

    Implement join game action

--- join.go
+++ join.go
@@ -1,9 +1,15 @@
 package main
 
-import (
-	"github.com/gorilla/websocket"
-)
+import "github.com/gorilla/websocket"
 
 func joinGame(games map[int]*Game, ws *websocket.Conn, msg Message) {
-
+	game := games[msg.GameId]
+	if game != nil && (ws == game.Players[0] || ws == game.Players[1]) {
+		sendWsMessage(ws, game)
+		return
+	}
+	if game != nil && game.Players[1] == nil {
+		game.Players[1] = ws
+		sendWsMessage(ws, game)
+	}
 }
